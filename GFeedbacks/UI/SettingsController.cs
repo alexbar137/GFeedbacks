38e20b47e3203ce2c4ff70f46d2dc7c34110eda8
Alexander Barabash <avbarabash@outlook.com>
2018-01-30 22:51:35 +0700

    Wpf addede

--- GFeedbacks/UI/SettingsController.cs
+++ GFeedbacks/UI/SettingsController.cs
@@ -1,41 +1,39 @@
 ﻿using GFeedbacks.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using GFeedbacks.Annotations;
 
 namespace GFeedbacks.UI
 {
-    public class SettingsController
+    public class SettingsController: INotifyPropertyChanged
     {
-        public string CurrentProfileName { get; set; }
-        public IAppSettings Settings
+        private string _currentProfileName;
+        public string CurrentProfileName
         {
-            get
+            get { return _currentProfileName; }
+            set
             {
-                return Globals.ThisAddIn.Settings;
+                _currentProfileName = value;
+                OnPropertyChanged(nameof(CurrentProfileName));
+                OnPropertyChanged(nameof(CurrentProfile));
+
             }
         }
+        public IAppSettings Settings => Globals.ThisAddIn.Settings;
 
-        public ISetting CurrentProfile
+        public ISetting CurrentProfile => Settings.FirstOrDefault(s => s.Name == CurrentProfileName);
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
-            get
-            {
-                return Settings.FirstOrDefault(s => s.Name == CurrentProfileName);
-            }
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
-
-        public SettingsController()
-        {
-            ShowForm();
-        }
-
-        public void ShowForm()
-        {
-            SettingsForm form = new SettingsForm(this);
-            form.Show();
-        }
-
     }
 }
